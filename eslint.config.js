b1803739d716b959e15d5af774c5f59c84c961f2
Michael Quintero <mqad89@gmail.com>
2026-08-30 12:19:22 -0400

    chore(storybook): initialize storybook with react and vite

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 // eslint.config.mjs
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
@@ -6,8 +9,10 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  eslintConfigPrettier, // Apaga las reglas de ESLint que interfieren con Prettier
+  // Apaga las reglas de ESLint que interfieren con Prettier
+  eslintConfigPrettier,
   {
     ignores: ['**/dist/**', '**/node_modules/**'],
   },
+  storybook.configs['flat/recommended'],
 );
